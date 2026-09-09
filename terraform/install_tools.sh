7f858be88fe3118f5a55ccbcb493158110608837
Ubuntu <ubuntu@ip-172-31-10-92.ap-south-1.compute.internal>
2026-09-09 10:22:42 +0000

    ec2.tf Update

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -70,7 +70,7 @@ sudo systemctl restart jenkins
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
-argocd version --client
+argocd version --client >> /var/log/install-tools.log
 
 echo "===== ALL TOOLS INSTALLED SUCCESSFULLY =====" >>/var/log/install-tools.log
 
